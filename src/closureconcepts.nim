074909c8fcf92ffa1015c22a65d8969c2a1d35f2
sls1005 <sls1005@github.com>
2023-10-10 14:56:19 +0800

    Add a comment

--- src/closureconcepts.nim
+++ src/closureconcepts.nim
@@ -12,3 +12,5 @@ type
     x is SomeClosure
     x isnot ClosureIterator # i.e. not compiles(system.finished(x))
     (x is proc)
+
+#It works because the standard implentations of `rawProc`, `rawEnv` and `finished` would raise a compile-time error if an argument doesn't fit the requirements.
